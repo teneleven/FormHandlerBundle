1e7da677fa7e4f40f3dd81360c1df50f49533285
Justin Hilles <justin@1011i.com>
2013-12-08 19:57:11 -0800

    add sample contact form

---
+++ Form/ContactType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Teneleven\Bundle\FormHandlerBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ContactType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('first_name')
+            ->add('last_name')
+            ->add('email')
+            ->add('comment', 'text')
+            ->add('file', 'file')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => null
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'contact_form';
+    }
+}
